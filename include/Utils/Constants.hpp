c77cde8f3b7d5b4322804dac2e7ba1913d318d74
AbolfazlAsali <asali.project006@gmail.com>
2026-08-15 16:49:37 +0330

    [feat: Utils]: add default file paths for capacities, incentives, express line, trains, passengers.

--- include/Utils/Constants.hpp
+++ include/Utils/Constants.hpp
@@ -11,6 +11,11 @@ namespace QomMetro::Utils {
     inline constexpr int kInvalid_StationId = -1;
 
     // Default file paths (can be overridden by CLI args later).
-    inline constexpr const char* Default_Stations_File = "file_path....";
-    inline constexpr const char* Default_Edges_File = "file_path....";
+    inline constexpr const char* Default_Stations_File = "data/stations.json";
+    inline constexpr const char* Default_Edges_File = "data/edges.json";
+    inline constexpr const char* Default_Capacities_File = "data/capacities.json";
+    inline constexpr const char* Default_Incentives_File = "data/incentives.json";
+    inline constexpr const char* Default_ExpressEdges_File = "data/express_graph.json";
+    inline constexpr const char* Default_Trains_File = "data/trains.json";
+    inline constexpr const char* Default_Passengers_File = "data/passengers.json";
 } 
