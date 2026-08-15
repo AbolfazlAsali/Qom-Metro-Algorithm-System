81b5dd7aba7a01de2b3fc225f6c1928870398394
AbolfazlAsali <asali.project006@gmail.com>
2026-08-15 17:04:42 +0330

    [feat: IO]: extend FileLoader interface with loadCapacities, loadIncentives, loadDirectedGraph.

--- include/IO/File_Loader.hpp
+++ include/IO/File_Loader.hpp
@@ -19,6 +19,33 @@ public:
         const std::string& edges_file_path
 
     ) const = 0;
+
+
+
+    virtual void load_Capacities(
+
+        const std::string& capacities_file_path,
+        Core::Graph& graph
+    
+    ) const = 0;
+
+
+
+    virtual void load_Incentives(
+
+        const std::string& incentives_file_path,
+        Core::Graph& graph
+    
+    ) const = 0;
+
+
+
+    virtual std::unique_ptr<Core::Graph> load_Directed_Graph(
+
+        const std::string& stations_file_path,
+        const std::string& edges_file_path
+    
+    ) const = 0;
 };
 
 } // namespace QomMetro::IO
